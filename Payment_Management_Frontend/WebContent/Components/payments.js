8c1932fe2432b392194c7b3b38bdd0127d12f84f
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-05-14 23:39:29 +0530

    save function on payments.js

--- Payment_Management_Frontend/WebContent/Components/payments.js
+++ Payment_Management_Frontend/WebContent/Components/payments.js
@@ -4,3 +4,36 @@ $(document).ready(function()
 	$("#alertError").hide();
 });
 
+//Save ==================================
+$(document).on("click", "#btnSave", function(event) 
+{
+	// Clear alerts---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+	
+	// Form validation-------------------
+	var status = validateComplaintForm();
+	if (status != true) 
+	{
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	
+	// If valid------------------------
+	var type = ($("#hidPaymentIDSave").val() == "") ? "POST" : "PUT";
+	
+	$.ajax(
+		{
+			url: "PaymentsAPI",
+			type: type,
+			data: $("#formPayment").serialize(),
+			dataType: "text",
+			complete: function(response, status) 
+			{
+				onComplaintSaveComplete(response.responseText, status);
+			}
+		});
+});
